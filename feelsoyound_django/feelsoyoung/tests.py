bf7765f2e8ce6921d354d216a85ac8c2ee2801ef
Tianea2160 <rhlehfndvkd7557@daum.net>
2022-05-13 21:15:44 +0900

    feat feelsoyoung : add rest api (method : post)
    their url is
    [POST] http://localhost:8000/sentiment/recommend/
    and need request body content, image(nullable)

--- feelsoyound_django/feelsoyoung/tests.py
+++ feelsoyound_django/feelsoyoung/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
 
 # Create your tests here.
+from sentiment_analysis import sentiment
+
+sentiment_result = sentiment()
+title = list(sentiment_result['title'])
+artist = list(sentiment_result['artist'])
+
+res = {"song": {}}
+for a, t in title, artist:
+    res["song"][a] = t
+
+print(res)
--- feelsoyound_django/feelsoyoung/views.py
+++ feelsoyound_django/feelsoyoung/views.py
@@ -1,16 +1,36 @@
 from django.shortcuts import render
 from sentiment_analysis import sentiment
+from rest_framework.response import Response
+from rest_framework.status import HTTP_200_OK
+from rest_framework.decorators import api_view
+
 
 # Create your views here.
 
-def sentiment_views(request) :
+def sentiment_views(request):
     sentiment_result = sentiment()
     title = list(sentiment_result['title'])
     artist = list(sentiment_result['artist'])
-    for t in title :
-        print(t)
-    for a in artist :
-        print(a)
+    # for t in title:
+    #     print(t)
+    # for a in artist:
+    #     print(a)
 
-    content = {'title' : title, 'artist' : artist}
-    return render(request , 'sentiment_analysis.html',content)
+    content = {'title': title, 'artist': artist}
+    return render(request, 'sentiment_analysis.html', content)
+
+
+@api_view(["POST"])
+def post(request):
+    content = request.data["content"]
+    img = request.data["image"]
+
+    print(content, img)
+
+    sentiment_result = sentiment()
+    title = list(sentiment_result['title'])
+    artist = list(sentiment_result['artist'])
+
+    content = {'title': title, 'artist': artist}
+
+    return Response(status=HTTP_200_OK, data=content)
