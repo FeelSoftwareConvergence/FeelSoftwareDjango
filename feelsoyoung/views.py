04cf91d3b1fcf12433cccc839cc8608087cdd284
hyejinkwon <67617475+heajinkwon@users.noreply.github.com>
2022-06-02 01:27:11 +0900

    response img

--- feelsoyoung/views.py
+++ feelsoyoung/views.py
@@ -1,25 +1,13 @@
 from django.shortcuts import render
 from sentiment_analysis import sentiment
+from sentiment_analysis import recommend_music
+from image_emotion import emotion_from_image
 from rest_framework.response import Response
 from rest_framework.status import HTTP_200_OK
 from rest_framework.decorators import api_view
 
 
 # Create your views here.
-
-def sentiment_views(request):
-    sentiment_result = sentiment()
-    title = list(sentiment_result['title'])
-    artist = list(sentiment_result['artist'])
-    # for t in title:
-    #     print(t)
-    # for a in artist:
-    #     print(a)
-
-    content = {'title': title, 'artist': artist}
-    return render(request, 'sentiment_analysis.html', content)
-
-
 @api_view(["POST"])
 def post(request):
     reply = request.data["content"]
@@ -28,7 +16,11 @@ def post(request):
     print(reply)
     print(img)
 
-    sentiment_result = sentiment(reply)
+    text_result = sentiment(reply)
+    img_result = emotion_from_image(img)
+
+    sentiment_result = recommend_music(reply, text_result, img_result)
+
     title = list(sentiment_result['title'])
     artist = list(sentiment_result['artist'])
 
