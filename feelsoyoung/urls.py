4dbe4a577d849ace9e10c18d9a3893708f460e13
tianea2160 <rhlehfndvkd7557@daum.net>
2022-06-03 07:42:56 +0000

    feat content and image upload : using form data image and text can be transmited

--- feelsoyoung/urls.py
+++ feelsoyoung/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from feelsoyoung import views
+from django.conf import settings
+from django.conf.urls.static import static
 
-urlpatterns = [
+
+urlpatterns = [ 
     #path("sentiment_analysis/", views.sentiment_views),
     path("recommend/", views.post),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- feelsoyoung/views.py
+++ feelsoyoung/views.py
@@ -5,25 +5,37 @@ from image_emotion import emotion_from_image
 from rest_framework.response import Response
 from rest_framework.status import HTTP_200_OK
 from rest_framework.decorators import api_view
-
+from django.core.files.storage import default_storage
+from django.core.files.base import ContentFile
+from django.conf import settings
+import os
+import glob
 
 # Create your views here.
 @api_view(["POST"])
 def post(request):
-    reply = request.data["content"]
-    img = request.data["image"]
+    image = request.FILES["image"]
+    content = request.POST["content"]
 
-    print(reply)
-    print(img)
+    path = default_storage.save('target_image.png', ContentFile(image.read()))
+    tmp_file = os.path.join(settings.MEDIA_ROOT, path)
 
-    text_result = sentiment(reply)
-    img_result = emotion_from_image(img)
+    print(content)  
+    print("path: " + tmp_file)
 
-    sentiment_result = recommend_music(reply, text_result, img_result)
+    text_result = sentiment(content)
+    img_result = emotion_from_image(tmp_file)
+
+    sentiment_result = recommend_music(content, text_result, img_result)
 
     title = list(sentiment_result['title'])
     artist = list(sentiment_result['artist'])
 
     content = {'title': title, 'artist': artist}
+    
+    # delete image
+    trash_image = glob.glob(settings.MEDIA_ROOT + '/*.png')
+    for i in trash_image:
+        os.remove(i)
 
     return Response(status=HTTP_200_OK, data=content)
